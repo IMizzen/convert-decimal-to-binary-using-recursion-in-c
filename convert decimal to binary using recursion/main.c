ad9babb9d268ed0a9085028dab73b59ccea25e67
Mazhar <71794610+IMizzen@users.noreply.github.com>
2022-05-01 03:31:12 +0300

    Convert to decimal to binary
    convert decimal to binary using recursion

---
+++ convert decimal to binary using recursion/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int find(int decimal_number)
+{
+
+    if (decimal_number == 0)
+        return 0;
+    else
+        return (decimal_number % 2 + 10 *
+                find(decimal_number / 2));
+}
+
+
+int main()
+{
+    int decimal_number;
+    printf("Please enter a number: ");
+    scanf("%d", &decimal_number);
+
+    printf("%d", find(decimal_number));
+
+    getch();
+    return 0;
+}
